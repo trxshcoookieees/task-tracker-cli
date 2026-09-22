facb3d27d4cbf17b60c60e28ede612d7331da663
Maxim Abdreikin <maximabdreikin@gmail.com>
2026-09-22 15:43:31 +0700

    feat: Switch to classes

--- src/taskcli/cli.py
+++ src/taskcli/cli.py
@@ -1,48 +1,70 @@
 import argparse
 import json
+from dataclasses import dataclass, asdict
+from enum import Enum
 from datetime import datetime
 from pathlib import Path
+
+
+class TaskStatus(str, Enum):
+    TODO = "todo"
+    IN_PROGRESS = "in-progress"
+    DONE = "done"
+
+
+@dataclass
+class Task:
+    id: int
+    name: str
+    created_at: str
+    updated_at: str
+    description: str | None = None
+    status: TaskStatus = TaskStatus.TODO
+
 
 TASKS_FILE = Path.home() / "taskcli.json"
 
 
-def load_tasks():
+def load_tasks() -> list[Task]:
     if not TASKS_FILE.exists():
         return []
     try:
         with TASKS_FILE.open("r") as file:
-            return json.load(file)
+            data = json.load(file)
+            return [Task(**task) for task in data]
     except json.JSONDecodeError:
         print("Error: task file contains invalid JSON")
         return []
 
 
-def save_tasks(tasks):
+def save_tasks(tasks: list[Task]) -> None:
     with TASKS_FILE.open("w") as file:
-        json.dump(tasks, file, indent=4)
+        json.dump(
+            [asdict(task) for task in tasks],
+            file,
+            indent=4
+        )
 
 
-def add_task(args):
-    description = args.description if args.description else ""
+def add_task(args) -> None:
+    description = args.description
     tasks = load_tasks()
-    new_id = max((task["id"] for task in tasks), default=0) + 1
+    new_id = max((task.id for task in tasks), default=0) + 1
     date_now = datetime.now().isoformat()
-    new_task = {
-        "id": new_id,
-        "name": args.name,
-        "description": description,
-        "status": "todo",
-        "createdAt": date_now,
-        "updatedAt": date_now,
-    }
+    new_task = Task(
+        id=new_id,
+        name=args.name,
+        description=description,
+        created_at=date_now,
+        updated_at=date_now
+    )
     tasks.append(new_task)
-
     save_tasks(tasks)
 
     print(f"Task added successfully (ID: {new_id})")
 
 
-def update_task(args):
+def update_task(args) -> None:
     task_id = args.id
 
     tasks = load_tasks()
@@ -53,15 +75,15 @@ def update_task(args):
         return
 
     if args.name is not None:
-        task["name"] = args.name
+        task.name = args.name
     if args.description is not None:
-        task["description"] = args.description
-    task["updatedAt"] = datetime.now().isoformat()
+        task.description = args.description
+    task.updated_at = datetime.now().isoformat()
     save_tasks(tasks)
     print(f"Task updated successfully (ID: {task_id})")
 
 
-def delete_task(args):
+def delete_task(args) -> None:
     task_id = args.id
     tasks = load_tasks()
     task = find_task(tasks, task_id)
@@ -75,7 +97,7 @@ def delete_task(args):
     print(f"Task deleted successfully (ID: {task_id})")
 
 
-def list_tasks(args):
+def list_tasks(args) -> None:
     status = args.filter
     tasks = load_tasks()
     print("-------TASKS--------")
@@ -83,21 +105,20 @@ def list_tasks(args):
         print(f"Filter: {status}\n")
         tasks = [
             task for task in tasks
-            if task["status"].lower() == status
+            if task.status == status
         ]
-    print()
     for task in tasks:
-        updated_at = datetime.fromisoformat(task["updatedAt"])
+        updated_at = datetime.fromisoformat(task.updated_at)
         print("--------------------")
-        print(f"{task['id']}: {task['name']}")
-        print(f"Description: {task['description']}")
-        print(f"Status: {task['status']}")
+        print(f"{task.id}: {task.name}")
+        print(f"Description: {task.description}")
+        print(f"Status: {task.status}")
         print(f"Last change: {updated_at.strftime("%d %B %Y, %H:%M")}")
 
     print("--------------------")
 
 
-def mark_done(args):
+def mark_status(args) -> None:
     task_id = args.id
 
     tasks = load_tasks()
@@ -107,34 +128,19 @@ def mark_done(args):
         print(f"Task not found (ID: {task_id})")
         return
 
-    update_status(task, "done")
+    update_status(task, args.status)
     save_tasks(tasks)
     print(f"Task updated successfully (ID: {task_id})")
 
 
-def mark_in_progress(args):
-    task_id = args.id
-
-    tasks = load_tasks()
-    task = find_task(tasks, task_id)
-
-    if task is None:
-        print(f"Task not found (ID: {task_id})")
-        return
-
-    update_status(task, "in-progress")
-    save_tasks(tasks)
-    print(f"Task updated successfully (ID: {task_id})")
+def update_status(task: Task, status: TaskStatus) -> None:
+    task.status = status
+    task.updated_at = datetime.now().isoformat()
 
 
-def update_status(task: dict, status):
-    task["status"] = status
-    task["updatedAt"] = datetime.now().isoformat()
-
-
-def find_task(tasks, task_id):
+def find_task(tasks: list[Task], task_id: int) -> Task | None:
     return next(
-        (task for task in tasks if task["id"] == task_id),
+        (task for task in tasks if task.id == task_id),
         None
     )
 
@@ -225,7 +231,10 @@ def main():
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
     mark_done_parser.add_argument("id", type=int, help="task ID to mark as done")
-    mark_done_parser.set_defaults(func=mark_done)
+    mark_done_parser.set_defaults(
+        func=mark_status,
+        status=TaskStatus.DONE
+    )
 
     # Mark in-progress command
     mark_in_progress_parser = subparsers.add_parser(
@@ -236,7 +245,10 @@ def main():
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
     mark_in_progress_parser.add_argument("id", type=int, help="task ID to mark as in progress")
-    mark_in_progress_parser.set_defaults(func=mark_in_progress)
+    mark_in_progress_parser.set_defaults(
+        func=mark_status,
+        status=TaskStatus.IN_PROGRESS
+    )
 
     args = parser.parse_args()
     args.func(args)
