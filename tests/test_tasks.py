8c81bf8b2980c53b40ea955fcb5ded39e8aeac01
Maxim Abdreikin <maximabdreikin@gmail.com>
2026-09-23 22:37:24 +0700

    test(tasks): add fixtures

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -14,33 +14,41 @@ from taskcli.tasks import (
 
 
 @pytest.fixture
-def prepared_tasks(tmp_path, monkeypatch):
-    monkeypatch.setattr(storage, "TASKS_FILE", tmp_path / "tasks.json")
+def tasks_file(tmp_path, monkeypatch):
+    tasks_file = tmp_path / "tasks.json"
+    monkeypatch.setattr(storage, "TASKS_FILE", tasks_file)
+    return tasks_file
 
+
+@pytest.fixture
+def tasks() -> list[Task]:
+    current_time = datetime.now().isoformat()
+
+    return [
+        Task(
+            id=1,
+            name="Task 1",
+            description="Task 1",
+            created_at=current_time,
+            updated_at=current_time,
+        ),
+        Task(
+            id=2,
+            name="Task 2",
+            description="Task 2",
+            created_at=current_time,
+            updated_at=current_time,
+        )
+    ]
+
+
+@pytest.fixture
+def prepared_tasks(tasks_file, tasks):
     for task in tasks:
         add_task(task.name, task.description)
 
 
-current_time = datetime.now().isoformat()
-tasks = [
-    Task(
-        id=1,
-        name="Task 1",
-        description="Task 1",
-        created_at=current_time,
-        updated_at=current_time,
-    ),
-    Task(
-        id=2,
-        name="Task 2",
-        description="Task 2",
-        created_at=current_time,
-        updated_at=current_time,
-    )
-]
-
-
-def test_find_existing_task():
+def test_find_existing_task(tasks):
     result = find_task(tasks, 2)
 
     assert result is not None
@@ -48,22 +56,21 @@ def test_find_existing_task():
     assert result.name == "Task 2"
 
 
-def test_find_missing_task():
+def test_find_missing_task(tasks):
     result = find_task(tasks, 1234)
 
     assert result is None
 
 
-def test_add_task(prepared_tasks):
+def test_add_task(tasks_file):
+    add_task("New task", "New description")
+
     result = storage.load_tasks()
 
-    assert len(result) == 2
-    assert result[0].name == "Task 1"
-    assert result[0].description == "Task 1"
+    assert len(result) == 1
+    assert result[0].name == "New task"
+    assert result[0].description == "New description"
     assert result[0].status == TaskStatus.TODO
-    assert result[1].name == "Task 2"
-    assert result[1].description == "Task 2"
-    assert result[1].status == TaskStatus.TODO
 
 
 def test_mark_status(prepared_tasks):
@@ -129,6 +136,7 @@ def test_delete_missing_task(prepared_tasks):
     assert result[0].name == "Task 1"
     assert result[1].name == "Task 2"
 
+
 def test_list_tasks(prepared_tasks, capsys):
     list_tasks(None)
 
@@ -143,5 +151,3 @@ def test_list_tasks(prepared_tasks, capsys):
     captured = capsys.readouterr()
     assert "1: Task 1" in captured.out
     assert "2: Task 2" not in captured.out
-
-
